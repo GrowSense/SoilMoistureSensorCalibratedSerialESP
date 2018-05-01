97f4165741cfbead13f057f0be42fdeb8bb935a6
John <compulsivecoder@gmail.com>
2018-05-01 23:56:48 +0000

    Fixed inject version script

--- inject-version.sh
+++ inject-version.sh
@@ -3,6 +3,6 @@ BUILD_NUMBER=$(cat buildnumber.txt)
 
 FULL_VERSION="$VERSION-$BUILD_NUMBER"
 
-SOURCE_FILE="src/SoilMoistureSensorCalibratedSerial/SoilMoistureSensorCalibratedSerial.ino"
+SOURCE_FILE="src/SoilMoistureSensorCalibratedSerialESP/SoilMoistureSensorCalibratedSerialESP.ino"
 
 sed -i "s/#define VERSION .*/#define VERSION \"$FULL_VERSION\"/" $SOURCE_FILE
