7faf085adbd346010e60b5d703768799b04efff8
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-26 15:27:58 +1000

    Added rollback functionality

---
+++ rollback.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if [ "$BRANCH" = "master" ]
+then
+  echo "Rolling back the master branch to previous revision"
+
+  echo "Pulling from origin/master" && \
+  git pull origin master && \
+
+  echo "Resetting to previous revision" && \
+  git reset --hard HEAD~1
+  
+  echo "Committing rollback"
+  git commit -am "Rolling back"
+  
+  echo "Pushing back to origin/master"
+  git push --force origin master
+
+  echo "The 'master' branch has been rolled back"  || \
+
+  (echo "Error" && exit 1)
+else
+  echo "You must be in the 'master' branch to perform a rollback. The 'dev' branch doesn't get rolled back automatically."
+fi
+
