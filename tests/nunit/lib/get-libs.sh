273f230ffef9fde5fbe9c081e7bc493f21cdcd5d
CompulsiveCoder <compulsivecoder@gmail.com>
2018-11-02 07:56:11 +0000

    Added M2Mqtt library

--- tests/nunit/lib/get-libs.sh
+++ tests/nunit/lib/get-libs.sh
@@ -11,5 +11,6 @@ sh nuget-install.sh NUnit.Runners 2.6.4
 sh nuget-install.sh Newtonsoft.Json 11.0.2
 sh nuget-install.sh ArduinoSerialControllerClient 1.0.9
 sh nuget-install.sh duinocom.core 1.0.6
+sh nuget-install.sh M2Mqtt 4.3.0.0
 
 echo "Finished getting library files."
