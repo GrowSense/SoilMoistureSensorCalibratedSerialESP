3f7752f44c918918989eb9115aa478fdbb79daf7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-05-25 13:27:17 +1000

    Trying to fix intermittent test failures

--- tests/nunit/src/SoilMoistureSensorCalibratedSerialESP.Tests.Integration/SerialOutputTimeTestHelper.cs
+++ tests/nunit/src/SoilMoistureSensorCalibratedSerialESP.Tests.Integration/SerialOutputTimeTestHelper.cs
@@ -19,7 +19,7 @@ namespace SoilMoistureSensorCalibratedSerialESP.Tests.Integration
             ReadFromDeviceAndOutputToConsole ();
 
             // Skip some data
-            WaitForData (3);
+            WaitForData (4);
 
             // Get the time until the next data line
             var secondsBetweenDataLines = WaitUntilDataLine ();
