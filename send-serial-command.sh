23d4c89f744fa8bbe06438cd42aea5f33253a718
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-25 11:35:14 +1000

    Updated and cleaned up scripts

--- send-serial-command.sh
+++ send-serial-command.sh
@@ -16,26 +16,9 @@ fi
 echo "  Command: $COMMAND"
 echo "  Device port: $SERIAL_PORT"
 
-#stty -F $SERIAL_PORT 9600 cs8 cread clocal
-
-#echo "WN:$WIFI_NETWORK" > $SERIAL_PORT
-
 exec 3<> $SERIAL_PORT
 
-#sleep 2
-
 echo "$COMMAND" >&3
-
-#sleep 2
-
-echo ""
-echo "--- Start Output from Device"
-
-cat <&3
-
-
-echo "--- End Output from Device"
-echo ""
 
 exec 3>&-
 
--- send-wifi-mqtt-commands.sh
+++ send-wifi-mqtt-commands.sh
@@ -14,7 +14,10 @@ MQTT_HOST=$(cat mqtt-host.security)
 MQTT_USERNAME=$(cat mqtt-username.security)
 MQTT_PASSWORD=$(cat mqtt-password.security)
 MQTT_PORT=$(cat mqtt-port.security)
-MQTT_DEVICE_NAME=$(cat mqtt-device-name.security)
+MQTT_DEVICE_NAME=""
+if [ -f "mqtt-device-name.security" ]; then
+  MQTT_DEVICE_NAME=$(cat mqtt-device-name.security)
+fi 
 
 echo "  WiFi Network: $WIFI_NAME"
 echo "  WiFi Password: [hidden]"
@@ -23,6 +26,9 @@ echo "  MQTT Host: $MQTT_HOST"
 echo "  MQTT Username: $MQTT_USERNAME"
 echo "  MQTT Password: [hidden]"
 echo "  MQTT Port: $MQTT_PORT"
+if [ $MQTT_DEVICE_NAME]; then
+  echo "  MQTT Device name: $MQTT_DEVICE_NAME"
+fi
 echo ""
 echo "  Device port: $SERIAL_PORT"
 echo ""
@@ -31,19 +37,21 @@ exec 3<> $SERIAL_PORT
 
 echo "  Sending..."
 echo "    WiFi Network"
-echo "WN:$WIFI_NAME\n" >&3
+echo "WN:$WIFI_NAME" >&3
 echo "    WiFi Password"
-echo "WPass:$WIFI_PASSWORD\n" >&3
+echo "WPass:$WIFI_PASSWORD" >&3
 echo "    MQTT Host"
-echo "MHost:$MQTT_HOST\n" >&3
+echo "MHost:$MQTT_HOST" >&3
 echo "    MQTT Username"
-echo "MUser:$MQTT_USERNAME\n" >&3
+echo "MUser:$MQTT_USERNAME" >&3
 echo "    MQTT Password"
-echo "MPass:$MQTT_PASSWORD\n" >&3
+echo "MPass:$MQTT_PASSWORD" >&3
 echo "    MQTT Port"
-echo "MPort:$MQTT_PORT\n" >&3
-echo "    MQTT Device name"
-echo "MDevice:$MQTT_DEVICE_NAME\n" >&3
+echo "MPort:$MQTT_PORT" >&3
+if [ $MQTT_DEVICE_NAME]; then
+  echo "    MQTT Device name"
+  echo "MDevice:$MQTT_DEVICE_NAME" >&3
+fi
 
 echo ""
 
