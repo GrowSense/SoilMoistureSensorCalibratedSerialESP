937b8147eea375284c479699ac520a80646bac46
CompulsiveCoder <compulsivecoder@gmail.com>
2019-11-29 07:18:07 +1100

    Added missing files

---
+++ src/SoilMoistureSensorCalibratedSerialESP/DeviceName.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <EEPROM.h>
+
+#include "DeviceName.h"
+#include "Common.h"
+#include "EEPROMHelper.h"
+
+#define DEVICE_NAME_EEPROM_FLAG_ADDRESS 210
+#define DEVICE_NAME_EEPROM_ADDRESS 211
+
+String deviceName = "wiFiMonitor1";
+
+void setDeviceName(char* newName)
+{
+  Serial.print("Device name: ");
+  Serial.println(newName);
+  
+  deviceName = newName;
+  
+  EEPROMWriteCharsAndSetFlag(DEVICE_NAME_EEPROM_FLAG_ADDRESS, DEVICE_NAME_EEPROM_ADDRESS, newName);
+}
+
+void loadDeviceNameFromEEPROM()
+{
+  if (isDebugMode)
+    Serial.println("  Reading device name from EEPROM");
+	  
+  if (EEPROMFlagIsSet(DEVICE_NAME_EEPROM_FLAG_ADDRESS))
+  {
+    deviceName = EEPROMReadString(DEVICE_NAME_EEPROM_ADDRESS);
+  }
+  else
+  {
+    if (isDebugMode)
+      Serial.println("  Device name has not been set to EEPROM. Using default.");
+  }
+
+  Serial.print("  Device name: ");
+  Serial.println(deviceName);
+}
+
---
+++ src/SoilMoistureSensorCalibratedSerialESP/DeviceName.h
@@ -0,0 +1,11 @@
+#ifndef DEVICENAME_H_
+#define DEVICENAME_H_
+
+extern String deviceName;
+
+void setDeviceName(char* newName);
+
+void loadDeviceNameFromEEPROM();
+
+#endif
+/* DEVICENAME */
